7a8b4c25415d7736c4a4f2e00524c641e0a199b3
sonhai32 <lamsonhai1379@gmail.com>
2021-01-25 01:18:08 +0700

    add color and theme config

---
+++ constants/Icons.ts
@@ -0,0 +1,17 @@
+export const calendar = require('../assets/icons/icons8-calendar.png');
+export const menu = require('../assets/icons/icons8-menu.png');
+export const score_card = require('../assets/icons/icons8-scorecard.png');
+export const user = require('../assets/icons/icons8-user.png');
+export const lock = require('../assets/icons/icons8-lock.png');
+export const key = require('../assets/icons/icons8-key.png');
+
+
+export default {
+    calendar,
+    menu,
+    score_card,
+    user,
+    lock,
+    key,
+
+}
